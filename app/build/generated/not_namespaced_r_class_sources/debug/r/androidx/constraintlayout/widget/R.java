bc9030e63ebc71b5e04df8e10221af547bae2b85
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-11-12 13:11:47 +0100

    Pantalla de inicio de sesion terminada

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,17 +74,17 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070021;
-        public static final int end = 0x7f070038;
-        public static final int gone = 0x7f07003f;
-        public static final int invisible = 0x7f070049;
-        public static final int left = 0x7f07004b;
-        public static final int packed = 0x7f070059;
-        public static final int parent = 0x7f07005a;
-        public static final int percent = 0x7f07005c;
-        public static final int right = 0x7f070060;
-        public static final int spread = 0x7f070078;
-        public static final int spread_inside = 0x7f070079;
-        public static final int start = 0x7f07007e;
+        public static final int end = 0x7f070037;
+        public static final int gone = 0x7f07003e;
+        public static final int invisible = 0x7f070048;
+        public static final int left = 0x7f07004a;
+        public static final int packed = 0x7f070058;
+        public static final int parent = 0x7f070059;
+        public static final int percent = 0x7f07005b;
+        public static final int right = 0x7f07005f;
+        public static final int spread = 0x7f070077;
+        public static final int spread_inside = 0x7f070078;
+        public static final int start = 0x7f07007d;
         public static final int top = 0x7f07008e;
         public static final int wrap = 0x7f070094;
     }
--- app/src/main/java/com/example/proyectoprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/proyectoprimeraaplicacion/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.proyectoprimeraaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString("usuario", "admin");
+        editor.putString("contrasena", "admin");
+        editor.commit();*/
+    }
+
+    public void iniciarSesion(View view){
+        SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+        EditText usuario = (EditText) findViewById(R.id.textUsuario);
+        EditText contrasena = (EditText) findViewById(R.id.textContrasena);
+
+        if(preferencias.getString("usuario","").equalsIgnoreCase(usuario.getText().toString()) && preferencias.getString("contrasena","").equalsIgnoreCase(contrasena.getText().toString())){
+            Intent i = new Intent(this, PantallaPrincipal.class);
+            startActivity(i);
+        }
+        else{
+            Toast notificacion = Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_LONG);
+            notificacion.show();
+        }
     }
 }
