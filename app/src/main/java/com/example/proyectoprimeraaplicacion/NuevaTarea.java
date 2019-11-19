9a46aefde7afdc60a10efcd309e86259978ec350
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-11-19 14:18:26 +0100

    Hechas algunas funciones del panel de nueva tarea

--- app/src/main/java/com/example/proyectoprimeraaplicacion/NuevaTarea.java
+++ app/src/main/java/com/example/proyectoprimeraaplicacion/NuevaTarea.java
@@ -3,9 +3,11 @@ package com.example.proyectoprimeraaplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NuevaTarea extends AppCompatActivity {
@@ -26,5 +28,15 @@ public class NuevaTarea extends AppCompatActivity {
     public void cancelarNuevaTarea(View view){
         Intent i = new Intent(this, ListaTareasPendientes.class);
         startActivity(i);
+    }
+
+    public void crearNuevaTarea(View view){
+        BaseDatos tareas = new BaseDatos(this, "tareas", null, 1);
+        SQLiteDatabase bd = tareas.getWritableDatabase();
+        EditText textNombre = (EditText) findViewById(R.id.textNombre);
+        EditText textDescripcion = (EditText) findViewById(R.id.textDescripcion);
+
+        String nombre = textNombre.getText().toString();
+        String descripcion = textDescripcion.getText().toString();
     }
 }
