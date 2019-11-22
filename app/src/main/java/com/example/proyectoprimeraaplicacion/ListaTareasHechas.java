217d186a2fc9f23b61ec72e9584b8ddc18286c9b
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-11-22 14:20:13 +0100

    inicio de algo

--- app/src/main/java/com/example/proyectoprimeraaplicacion/ListaTareasHechas.java
+++ app/src/main/java/com/example/proyectoprimeraaplicacion/ListaTareasHechas.java
@@ -3,8 +3,12 @@ package com.example.proyectoprimeraaplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class ListaTareasHechas extends AppCompatActivity {
 
@@ -12,10 +16,23 @@ public class ListaTareasHechas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_tareas_hechas);
+
+        cargarTareas();
     }
 
     public void nuevaTareaH(View view){
         Intent i = new Intent(this, NuevaTarea.class);
         startActivity(i);
+    }
+
+    public void cargarTareas(){
+        ArrayList<Tarea> listaTareas = new ArrayList<>();
+        BaseDatos tareas = new BaseDatos(this, "tareas", null, 1);
+        SQLiteDatabase bd = tareas.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select * from tareas where hechas = false", null);
+
+        if(fila.moveToFirst()){
+
+        }
     }
 }
